7c94f62d1b75db85afa39abfb4d6e199b166ec26
Lee Keitel <lee@keitel.xyz>
2018-05-25 21:47:54 -0500

    Created patch to remove mshortcuts for mouse scrollback

--- config.def.h
+++ config.def.h
@@ -160,8 +160,8 @@ static unsigned int defaultattr = 11;
  */
 static MouseShortcut mshortcuts[] = {
 	/* button               mask            string */
-	{ Button4,              XK_NO_MOD,      "\031" },
-	{ Button5,              XK_NO_MOD,      "\005" },
+	// { Button4,              XK_NO_MOD,      "\031" },
+	// { Button5,              XK_NO_MOD,      "\005" },
 };
 
 MouseKey mkeys[] = {
